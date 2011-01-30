99319e9199d6c741fa6b8d36211b13eef4ea0746
Reza Alavi <rezaalavi@rubyforge.org>
2011-01-30 23:15:30 +1100

    Plugin is not Gem based

--- Rakefile
+++ Rakefile
@@ -3,5 +3,31 @@
 
 require File.expand_path('../config/application', __FILE__)
 require 'rake'
+require 'rake/gempackagetask'
+
+PKG_FILES = FileList[
+  'Vendor/**/*'
+]
+
+spec = Gem::Specification.new do |s|
+  s.name = "rails-mobile"
+  s.version = "0.9.1"
+  s.author = "Reza Alavi"
+  s.email = "rezaalavi@rubyforge.org"
+  s.homepage = "https://github.com/rezaalavi/Rails-Mobile"
+  s.platform = Gem::Platform::RUBY
+  s.rubyforge_project = "rails-mobile"
+  s.summary = "Rails mobile is a Rails plug-in that adds mobile device recognition and dynamic controller/action switching to Rails."
+  s.files = PKG_FILES.to_a
+  s.require_path = "lib"
+  s.has_rdoc = false
+  s.add_dependency('mobilesrepo')
+  s.extra_rdoc_files = ['README.rdoc']
+end
+
+desc 'Turn this plugin into a gem.'
+Rake::GemPackageTask.new(spec) do |pkg|
+  pkg.gem_spec = spec
+end
 
 RailsMobileApp::Application.load_tasks
--- vendor/plugins/rails_mobile/lib/rails_mobile.rb
+++ vendor/plugins/rails_mobile/lib/rails_mobile.rb
@@ -1,5 +1,22 @@
-require "rails_mobile/application"
-require "rails_mobile/mobile_dispatch/mobile_matcher"
-require "rails_mobile/mobile_dispatch/action_dispatch"
-require "rails_mobile/mobile_dispatch/categories"
-require "rails_mobile/mobile_dispatch/parameters"
+module RailsMobilePlugin
+  # namespace our plugin and inherit from Rails::Railtie
+  # to get our plugin into the initialization process
+  class Railtie < Rails::Railtie
+
+    # configure our plugin on boot. other extension points such
+    # as configuration, rake tasks, etc, are also available
+    initializer "railsmobilenewplugin.initialize" do |app|
+      require "rails_mobile/application"
+      require "rails_mobile/mobile_dispatch/mobile_matcher"
+      require "rails_mobile/mobile_dispatch/action_dispatch"
+      require "rails_mobile/mobile_dispatch/categories"
+      require "rails_mobile/mobile_dispatch/parameters"
+
+    end
+
+  end
+end
+
+
+
+
